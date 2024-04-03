285e10117d5fe35b9718b3e775567f42d67a7c3c
mozumasu <ouri1229@gmail.com>
2024-04-03 20:19:43 +0900

    config: ⚙ weztermの起動時にフルスクリーン化

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,11 +8,8 @@ end
 
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 -- colors
 -- config.color_scheme = "AdventureTime"
@@ -22,6 +19,12 @@ config.window_background_opacity = 0.8
 config.font = wezterm.font("HackGen")
 config.font_size = 13.0
 
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
 
 -- and finally, return the configuration to wezterm
 return config
