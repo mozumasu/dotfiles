ed3caa296b761aa1c0a48154ae9a4bd623e0c753
mozumasu <ouri1229@gmail.com>
2024-08-26 09:39:18 +0900

    style: 💄 [wezterm/starship]

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,12 +13,18 @@ config.key_tables = require("keybinds").key_tables
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 -- colors
+--
 -- config.color_scheme = "AdventureTime"
 config.macos_window_background_blur = 20
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 
+-- config.window_background_gradient = {
+-- 	colors = { "#03001e", "#1565C0" },
+-- 	-- Specifices a Linear gradient starting in the top left corner.
+-- 	orientation = { Linear = { angle = -45.0 } },
+-- }
 -- font
-config.font = wezterm.font("HackGen")
+config.font = wezterm.font("Hack")
 config.font_size = 13.0
 
 -- 最初からフルスクリーンで起動
