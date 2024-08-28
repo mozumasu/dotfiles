257e2e31fabd13eaa6d17bea0dc8350e781d5c8e
mozumasu <ouri1229@gmail.com>
2024-08-28 10:38:32 +0900

    feat: 🎸 [nvim]solarized-osakaカラーを追加

---
+++ .config/nvim/lua/plugins/solarized-osaka.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "osaka",
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
