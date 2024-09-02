c9f78349a5db66513d141e2f94e605309092fa88
mozumasu <ouri1229@gmail.com>
2024-09-02 18:28:39 +0900

    feat: 🎸 [nvim]yamlでもCopilotの補完が聞くように設定

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      yaml = true,
+      help = true,
+    },
+  },
+}
