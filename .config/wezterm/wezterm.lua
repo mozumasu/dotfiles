ca6b7ae0837a16c1cbad1cace00f48578a2fbc7c
mozumasu <ouri1229@gmail.com>
2024-09-08 21:14:51 +0900

    feat: 🎸 [wezterm]タブバーの見た目を変更

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,41 +1,72 @@
 local wezterm = require("wezterm")
-
 local config = {}
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- local scheme = wezterm.get_builtin_color_schemes()["nord"]
+config.automatically_reload_config = true
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+
+config.window_padding = {
+  bottom = 0,
+}
+
+config.font_size = 12.0
+
 ----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- config.tab_bar_at_bottom = false
--- The filled in variant of the < symbol
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+-- falseにするとタブバーの透過が効かなくなる
+-- config.use_fancy_tab_bar = false
+-- タブバーを透過する
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
 
+config.window_frame = {
+  active_titlebar_bg = "none",
+}
+
+-- config.tab_bar.inactive_tab_edge = "none"
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+    active_tab = {
+      bg_color = "none",
+      fg_color = "none",
+    },
+  },
+}
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 -- The filled in variant of the > symbol
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  -- local tab_index = tab.tab_index + 1
-  local edge_background = "#050606"
-  local background = "#1b1032"
-  local foreground = "#808080"
+  local edge_background = "none"
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
 
   if tab.is_active then
-    background = "#2b2042"
-    foreground = "#c0c0c0"
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
     -- Copy mode
   elseif hover then
     background = "#3b3052"
-    foreground = "#909090"
+    foreground = "#FFFFFF"
   end
 
   local edge_foreground = background
 
-  -- ensure that the titles fit in the available space,
-  -- and that we have room for the edges.
-  local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
+  -- local title = wezterm.truncate_right(tab.active_pane.title, max_width - 2)
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
 
   return {
     -- { string.format(" %d ", tab_index) },
@@ -59,25 +90,6 @@ config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
 config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
-----------------------------------------------------
--- colors
-----------------------------------------------------
--- config.color_scheme = "#041319"
--- config.color_scheme = "AdventureTime"
-config.macos_window_background_blur = 20
-config.window_background_opacity = 0.9
-
--- config.window_background_gradient = {
--- 	colors = { "#03001e", "#1565C0" },
--- 	-- Specifies a Linear gradient starting in the top left corner.
--- 	orientation = { Linear = { angle = -45.0 } },
--- }
-----------------------------------------------------
--- font
-----------------------------------------------------
--- config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 13.0
-
 -- 最初からフルスクリーンで起動
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
@@ -85,5 +97,4 @@ wezterm.on("gui-startup", function(cmd)
   window:gui_window():toggle_fullscreen()
 end)
 
--- and finally, return the configuration to wezterm
 return config
