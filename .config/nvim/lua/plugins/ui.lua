0341350314fa267f4e6677b97302976546da6477
mozumasu <ouri1229@gmail.com>
2024-09-09 15:04:34 +0900

    feat: 🎸 [nvim]それぞれのpaneにファイル名を表示する

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,36 @@
+return {
+  -- filename
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base4 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          -- 2つ上の階層までディレクトリを表示
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":p:~:.:h:h")
+            .. "/"
+            .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { "" }, { filename } }
+        end,
+      })
+    end,
+  },
+}
