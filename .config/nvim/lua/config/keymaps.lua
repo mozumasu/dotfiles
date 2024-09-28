e734967022e6a09df66ddd6c29c9ecfc59a9ce67
mozumasu <ouri1229@gmail.com>
2024-09-28 17:27:16 +0900

    feat: 🎸 [nvim]ターミナルのキーバインドを変更

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,43 +1,68 @@
-local keymap = vim.keymap
+local keymap = vim.keymap.set
+local keydel = vim.keymap.del
+
 local opts = { noremap = true, silent = true }
+local util = require("lazyvim.util")
 
 -- Control + I と Tab をデフォルトの状態に戻す
 vim.api.nvim_set_keymap("n", "<C-i>", "<C-i>", { noremap = true })
 vim.api.nvim_set_keymap("i", "<C-i>", "<C-i>", { noremap = true })
 
 -- Increment / Decrement
-keymap.set("n", "+", "<C-a>", opts)
-keymap.set("n", "-", "<C-x>", opts)
+keymap("n", "+", "<C-a>", opts)
+keymap("n", "-", "<C-x>", opts)
 
 -- Select all
-keymap.set("n", "<C-a>", "ggVG")
+keymap("n", "<C-a>", "ggVG")
 
 -- Tab
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap("n", "<tab>", ":tabnext<Return>", opts)
+keymap("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap("n", "ss", ":split<Return>", opts)
+keymap("n", "sv", ":vsplit<Return>", opts)
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+keymap("n", "sh", "<C-w>h")
+keymap("n", "sk", "<C-w>k")
+keymap("n", "sj", "<C-w>j")
+keymap("n", "sl", "<C-w>l")
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+keymap("n", "<C-w><left>", "<C-w><")
+keymap("n", "<C-w><right>", "<C-w>>")
+keymap("n", "<C-w><up>", "<C-w>+")
+keymap("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostics
-keymap.set("n", "<C-j>", function()
+keymap("n", "<C-j>", function()
   vim.diagnostic.goto_next()
 end, opts)
 
 -- lazydocker
 if vim.fn.executable("lazydocker") == 1 then
   vim.keymap.set("n", "<leader>d", function()
-    LazyVim.terminal("lazydocker", { esc_esc = false, ctrl_hjkl = false })
+    util.terminal("lazydocker", { esc_esc = false, ctrl_hjkl = false, border = "rounded" })
   end, { desc = "LazyDocker" })
 end
+
+-- terminal
+local lazyterm = function()
+  util.terminal(nil, { cwd = util.root(), border = "rounded" })
+end
+
+-- floating terminal
+keymap("n", "<c-_>", lazyterm, { desc = "Terminal (cwd)" })
+keymap("n", "<c-/>", function()
+  util.terminal(nil, { border = "double" })
+end, { desc = "Terminal (root)" })
+keymap("t", "<esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+keymap("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+keymap("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- disable default keymappings
+keydel("n", "<leader>ft", { desc = "Terminal (cwd)" })
+keydel("n", "<leader>fT", { desc = "Terminal (root)" })
+keydel("t", "<C-h>", { desc = "Go to Left Window" })
+keydel("t", "<C-j>", { desc = "Go to Lower Window" })
+keydel("t", "<C-k>", { desc = "Go to Upper Window" })
+keydel("t", "<C-l>", { desc = "Go to Right Window" })
