1f8ed7ec66f995effeb76797f09c7e751f45aa91
mozumasu <ouri1229@gmail.com>
2024-09-30 15:02:53 +0900

    feat: 🎸 [nvim]copilotをLazyExtrasの設定を使わないようにする

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -37,7 +37,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
