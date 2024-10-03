7919d746ae69ef0407e13c6f7df0bd805eef47fd
mozumasu <ouri1229@gmail.com>
2024-10-03 15:20:56 +0900

    feat: 🎸 Adjusted floating window transparency

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,10 @@
 -- vim.opt.winbar = "%=%m %f"
 -- vim標準スペルチェックから日本語を除外
 vim.opt.spelllang:append("cjk")
+
+-- ターミナルでもTrue Colorを有効にする
+vim.opt.termguicolors = true
+-- floating windowの背景を透過
+vim.opt.winblend = 20
+-- 補完メニューの背景を透過
+vim.opt.pumblend = 50
