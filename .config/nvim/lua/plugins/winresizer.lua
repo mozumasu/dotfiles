f9d0ef61e0276152db9246b495350b5cd6920c88
mozumasu <ouri1229@gmail.com>
2024-10-03 19:44:27 +0900

    feat: 🎸 [nvim]Add Winresizer

---
+++ .config/nvim/lua/plugins/winresizer.lua
@@ -0,0 +1,7 @@
+return {
+  "simeji/winresizer",
+  keys = {
+    { "<leader>wr", "<cmd>WinResizerStartResize<cr>", mode = { "n", "v" }, desc = "Resize Window" },
+    { "<C-w>r", "<cmd>WinResizerStartResize<cr>", mode = { "n", "v" }, desc = "Resize Window" },
+  },
+}
