fcd2acafb311322f7cd6769217049d9ad015dfe7
mozumasu <ouri1229@gmail.com>
2024-10-03 15:22:00 +0900

    feat: 🎸 [nvim]Cange active window color

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,3 +9,27 @@ vim.opt.termguicolors = true
 vim.opt.winblend = 20
 -- 補完メニューの背景を透過
 vim.opt.pumblend = 50
+
+-- アクティブウィンドウの枠線の色を設定
+vim.cmd([[
+  hi ActiveWindowSeparator guifg=#69F5CD
+  hi InactiveWindowSeparator guifg=#555555
+]])
+
+-- アクティブウィンドウの枠線を変える
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+  callback = function()
+    vim.wo.winhighlight = "WinSeparator:ActiveWindowSeparator"
+  end,
+})
+
+-- 非アクティブウィンドウの枠線を変える
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    -- NeoTreeの場合は変更しない
+    local filetype = vim.bo.filetype
+    if filetype ~= "neo-tree" then
+      vim.wo.winhighlight = ""
+    end
+  end,
+})
