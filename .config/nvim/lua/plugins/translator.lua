c7858e0bf0a3af7499ce3606cfeb5325b5b322c7
mozumasu <ouri1229@gmail.com>
2024-10-05 13:08:20 +0900

    feat: 🎸 [nvim]Add options for vim-toranslator

--- .config/nvim/lua/plugins/translator.lua
+++ .config/nvim/lua/plugins/translator.lua
@@ -4,7 +4,6 @@ return {
     cmd = { "TranslateW", "TranslateW --target_lang=en" },
     keys = {
       -- Popup
-      -- Translate words into Japanese -- Translate lines into Japanese
       { "<leader>t", "", desc = "Translate" },
       { "<leader>tj", "<cmd>TranslateW<CR>", mode = "n", desc = "Translate words into Japanese" },
       { "<leader>tj", ":'<,'>TranslateW<CR>", mode = "v", desc = "Translate lines into Japanese" },
@@ -45,6 +44,9 @@ return {
       vim.g.translator_target_lang = "ja"
       vim.g.translator_default_engines = { "google" }
       vim.g.translator_history_enable = true
+      vim.g.translator_window_type = "popup"
+      vim.g.translator_window_max_width = 0.5
+      vim.g.translator_window_max_height = 0.9 -- 1 is not working
     end,
   },
 
