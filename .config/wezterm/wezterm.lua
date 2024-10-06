e6ca6b606d2fb24c3521950225bef0e795e1b9c4
mozumasu <ouri1229@gmail.com>
2024-10-06 15:17:37 +0900

    feat: 🎸 Add font setting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ config.use_ime = true
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 config.audible_bell = "Disabled"
+config.font = wezterm.font("HackGen Console NF")
 
 ----------------------------------------------------
 -- Tab
