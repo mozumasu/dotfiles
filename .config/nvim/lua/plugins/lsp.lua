73fb37c2806a09580212f96514a38c748483d9b7
mozumasu <ouri1229@gmail.com>
2024-10-10 10:23:05 +0900

    feat: 🎸 [nvim]Show which LSP the message is from

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,19 @@
 return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = {
+          source = "always", -- Always display lsp server name
+          prefix = "●",
+          format = function(diagnostic)
+            -- Add lsp server name to message
+            return string.format("%s (%s)", diagnostic.message, diagnostic.source or "Unknown")
+          end,
+        },
+      },
+    },
+  },
   require("lspconfig").typos_lsp.setup({
     init_options = {
       config = "~/.config/nvim/.typos.toml",
