03136d2ecb8acd77e2db1f9f5f67fe9d455cbd95
mozumasu <ouri1229@gmail.com>
2024-10-12 10:13:51 +0900

    feat: 🎸 [nvim]Add hlchunck

---
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,20 @@
+return {
+  "shellRaining/hlchunk.nvim",
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = ">",
+        },
+        style = "#806d9c",
+      },
+    })
+  end,
+}
