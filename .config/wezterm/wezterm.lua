4cacd7ce79b4abf5e9b251cfd9cda88156cba511
mozumasu <ouri1229@gmail.com>
2024-11-26 16:18:18 +0900

    feat: 🎸 [wezterm]background color settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,8 +4,8 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 13
 config.audible_bell = "Disabled"
 config.font = wezterm.font("HackGen Console NF")
 
