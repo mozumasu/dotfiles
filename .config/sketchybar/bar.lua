dfbc40a01283a6cfc9a54e84c384308a14c49a58
mozumasu <ouri1229@gmail.com>
2024-11-26 15:16:26 +0900

    refactor: 💡 [sketchybar]

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -2,11 +2,11 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	-- topmost = "window",
-	height = 44,
-	margin = 0,
-	color = 0x0000000,
-	y_offset = 0,
-	padding_right = 8,
-	padding_left = 8,
+  -- topmost = "window",
+  height = 44,
+  margin = 0,
+  color = 0x0000000,
+  y_offset = 0,
+  padding_right = 8,
+  padding_left = 8,
 })
