839b3ff8c8cde3db68cad36752cba960a1cb067b
mozumasu <ouri1229@gmail.com>
2024-12-19 21:13:46 +0900

    feat: 🎸 [nvim]Changed from Enter to Tab completion

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -42,7 +42,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    -- { import = "lazyvim.plugins.extras.ai.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
