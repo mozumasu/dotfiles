69ce56d434c653fde86ca8ad4872ffaaf334a330
mozumasu <ouri1229@gmail.com>
2025-01-31 14:08:56 +0900

    feat: 🎸 [zsh]history search

--- .config/zsh/rc/bindkey.zsh
+++ .config/zsh/rc/bindkey.zsh
@@ -6,3 +6,9 @@
 # Use emacs keybind
 bindkey -e
 bindkey '^g' ghq-fzf
+# history search
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^n" history-beginning-search-forward-end
