8e37ea52cdfede1c16ba9d2c74fbef8654411386
mozumasu <ouri1229@gmail.com>
2025-01-31 15:20:20 +0900

    feat: 🎸 [zsh]Add bindkey

--- .config/zsh/rc/bindkey.zsh
+++ .config/zsh/rc/bindkey.zsh
@@ -6,9 +6,22 @@
 # Use emacs keybind
 bindkey -e
 bindkey '^g' ghq-fzf
-# history search
+
+# History search
 autoload history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^p" history-beginning-search-backward-end
 bindkey "^n" history-beginning-search-forward-end
+
+# Smart Insert Last Word
+autoload smart-insert-last-word
+zle -N insert-last-word smart-insert-last-word
+bindkey "^[." insert-last-word
+
+# Edit Command Line
+autoload edit-command-line
+zle -N edit-command-line
+bindkey "^[e" edit-command-line
+
+bindkey '^U' push-line
