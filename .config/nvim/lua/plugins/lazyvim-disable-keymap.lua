41e3a8166853a772fcac2d5a3fd297719313bd09
mozumasu <ouri1229@gmail.com>
2025-02-16 10:29:11 +0900

    feat: ✨ remmove my config of Octo because use Octo settings of LazyVim

---
+++ .config/nvim/lua/plugins/lazyvim-disable-keymap.lua
@@ -0,0 +1,13 @@
+return {
+  "pwntester/octo.nvim",
+  keys = {
+    { "<leader>gS", false }, -- { "<leader>gS", "<cmd>Octo search<CR>", desc = "Search (Octo)", false },
+    { "<leader>a", false }, -- { "<leader>a", "", desc = "+assignee (Octo)", ft = "octo", false },
+    { "<leader>c", false }, -- { "<leader>c", "", desc = "+comment/code (Octo)", ft = "octo", false },
+    { "<leader>l", false }, -- { "<leader>l", "", desc = "+label (Octo)", ft = "octo", false },
+    { "<leader>ie", false }, -- { "<leader>ie", "<cmd>Octo issue edit<CR>", desc = "Edit issue", false },
+    { "<leader>r", false }, -- { "<leader>r", "", desc = "+react (Octo)", ft = "octo", false },
+    { "<leader>p", false }, -- { "<leader>p", "", desc = "+pr (Octo)", ft = "octo", false },
+    { "<leader>v", false }, -- { "<leader>v", "", desc = "+review (Octo)", ft = "octo", false },
+  },
+}
