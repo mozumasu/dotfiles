78ceedda5e403a7dc134efe128b2618d56598b6b
mozumasu <ouri1229@gmail.com>
2025-02-17 12:11:22 +0900

    refactor: ♻️ update Neovim configuration and add new key mappings

---
+++ .config/nvim/lua/plugins/ui/tokyonight.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    enable = false,
+    lazy = false, -- If it is not loaded at startup, it will not be added to the ui list.
+    opts = { style = "moon" },
+  },
+}
--- .config/nvim2/README.md
+++
@@ -1,4 +0,0 @@
-# 💤 Neovim
-
-- [folke/lazy.nvim: 💤 A modern plugin manager for Neovim](https://github.com/folke/lazy.nvim)
-- [🚀 Getting Started | LazyVim](https://www.lazyvim.org/)
