776007d4799960df10caf1d782456d5624e01d48
mozumasu <ouri1229@gmail.com>
2025-02-20 14:45:16 +0900

    feat: ✨ add initial configuration for snacks.nvim plugin support

---
+++ .config/nvim/lua/plugins/base/snacks.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    keys = {
+      {
+        "<leader>mp",
+        ft = { "png", "markdown" },
+        function()
+          Snacks.image.hover()
+        end,
+        desc = "Preview math formula under cursor",
+      },
+    },
+    ---@type snacks.Config
+    opts = {
+      image = {
+        enabled = true,
+        doc = {
+          -- enable image viewer for documents
+          -- a treesitter parser must be available for the enabled languages.
+          -- supported language injections: markdown, html
+          enabled = true,
+          -- render the image inline in the buffer
+          -- if your env doesn't support unicode placeholders, this will be disabled
+          -- takes precedence over `opts.float` on supported terminals
+          inline = false,
+          -- render the image in a floating window
+          -- only used if `opts.inline` is disabled
+          float = false,
+          max_width = 80,
+          max_height = 40,
+        },
+      },
+    },
+  },
+}
