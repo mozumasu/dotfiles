b0201a92f2be486044746d21356115f9c4ab9945
mozumasu <ouri1229@gmail.com>
2025-02-24 13:42:17 +0900

    feat(wezterm): ✨ enhance tab title customization and add pane color settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
 config.automatically_reload_config = true
 config.font_size = 13.0
@@ -10,67 +11,167 @@ config.audible_bell = "Disabled"
 config.font = wezterm.font("HackGen Console NF")
 
 ----------------------------------------------------
+-- Pane
+----------------------------------------------------
+config.inactive_pane_hsb = {
+  saturation = 1.0,
+  brightness = 0.5,
+}
+
+----------------------------------------------------
+-- Title Bar
+----------------------------------------------------
+-- Title bar not listed
+config.window_decorations = "RESIZE"
+
+----------------------------------------------------
 -- Tab
 ----------------------------------------------------
--- タイトルバーを非表示
-config.window_decorations = "RESIZE"
--- タブバーの表示
+-- Display tabs
 config.show_tabs_in_tab_bar = true
--- タブが一つの時は非表示
+-- If there is one tab, it is not displayed
 config.hide_tab_bar_if_only_one_tab = true
--- falseにするとタブバーの透過が効かなくなる
--- config.use_fancy_tab_bar = false
+-- Hide borders between tabs
+config.colors = {
+  tab_bar = {
+    background = "none",
+    inactive_tab_edge = "none",
+  },
+}
+config.show_new_tab_button_in_tab_bar = false
+-- Can only be used at nightly
+config.show_close_tab_button_in_tabs = false
+config.tab_max_width = 30
+-- Adjust the tab bar to the background color
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
 
--- タブバーの透過
+----------------------------------------------------
+-- Fansy Tab Bar (now, not use)
+----------------------------------------------------
+config.use_fancy_tab_bar = false
+-- Transparent tab bar
 config.window_frame = {
   inactive_titlebar_bg = "none",
   active_titlebar_bg = "none",
 }
 
--- タブバーを背景色に合わせる
-config.window_background_gradient = {
-  colors = { "#000000" },
-}
+----------------------------------------------------
+-- 各タブの「ディレクトリ名」を記憶しておくテーブル
+local title_cache = {}
 
--- タブの追加ボタンを非表示
-config.show_new_tab_button_in_tab_bar = false
--- nightlyのみ使用可能
--- タブの閉じるボタンを非表示
-config.show_close_tab_button_in_tabs = false
+wezterm.on("update-status", function(window, pane)
+  local pane_id = pane:pane_id()
+  title_cache[pane_id] = "-" -- default value
 
--- タブ同士の境界線を非表示
-config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
-}
+  local cwd_url = pane:get_current_working_dir()
+  if cwd_url then
+    local cwd = cwd_url.path -- `path` を取得
+    if cwd then
+      local home = os.getenv("HOME")
+      if home and cwd:find("^" .. home) then
+        cwd = cwd:gsub("^" .. home, "~")
+      end
 
--- タブの形をカスタマイズ
--- タブの左側の装飾
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
--- タブの右側の装飾
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+      -- Check if it follows the format /src/github.com/{user}/{project}/...
+      local github_prefix_pattern = ".*/src/github.com/([^/]+)/([^/]+)"
+      local user, project = cwd:match(github_prefix_pattern)
+
+      if user and project then
+        title_cache[pane_id] = project
+      else
+        -- Extract only the directory name
+        cwd = cwd:gsub("/$", "")
+        local last_dir = cwd:match("([^/]+)$")
+        title_cache[pane_id] = last_dir or cwd
+      end
+    end
+  end
+end)
+-- TAB ICONS
+local TAB_ICON_DOCKER = wezterm.nerdfonts.md_docker
+local TAB_ICON_PYTHON = wezterm.nerdfonts.dev_python
+local TAB_ICON_NEOVIM = wezterm.nerdfonts.linux_neovim
+local TAB_ICON_ZSH = wezterm.nerdfonts.dev_terminal
+local TAB_ICON_TASK = wezterm.nerdfonts.cod_server_process
+local TAB_ICON_NODE = wezterm.nerdfonts.md_language_typescript
+local TAB_ICON_FALLBACK = wezterm.nerdfonts.md_console_network
+-- TAB COLORE
+local TAB_ICON_COLOR_DOCKER = "#4169e1"
+local TAB_ICON_COLOR_PYTHON = "#ffd700"
+local TAB_ICON_COLOR_NEOVIM = "#57A143"
+local TAB_ICON_COLOR_ZSH = "#769ff0"
+local TAB_ICON_COLOR_TASK = "#ff7f50"
+local TAB_ICON_COLOR_NODE = "#1e90ff"
+local TAB_ICON_COLOR_FALLBACK = "#ae8b2d"
+local TAB_FOREGROUND_INACTIVE = "#a0a9cb"
+local TAB_BACKGROUND_INACTIVE = "#1d2230"
+local TAB_FOREGROUND_ACTIVE = "#313244"
+local TAB_BACKGROUND_ACTIVE = "#80EBDF"
+-- Tab fitting
+local SOLID_LEFT_CIRCLE = wezterm.nerdfonts.ple_left_half_circle_thick
+local SOLID_RIGHT_CIRCLE = wezterm.nerdfonts.ple_right_half_circle_thick
+-- local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+-- local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
-  local edge_background = "none"
+  local background = TAB_BACKGROUND_INACTIVE
+  local foreground = TAB_FOREGROUND_INACTIVE
   if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
+    background = TAB_BACKGROUND_ACTIVE
+    foreground = TAB_FOREGROUND_ACTIVE
   end
+  local edge_background = "none"
   local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  local pane = tab.active_pane
+  local pane_id = pane.pane_id
+
+  local cwd = "none"
+  if title_cache[pane_id] then
+    cwd = title_cache[pane_id]
+  else
+    cwd = "-"
+  end
+
+  local icon = TAB_ICON_FALLBACK
+  local icon_foreground = TAB_ICON_COLOR_FALLBACK
+  if tab.active_pane.title == "nvim" then
+    icon = TAB_ICON_NEOVIM
+    icon_foreground = TAB_ICON_COLOR_NEOVIM
+  elseif tab.active_pane.title == "zsh" then
+    icon = TAB_ICON_ZSH
+    icon_foreground = TAB_ICON_COLOR_ZSH
+  elseif tab.active_pane.title == "Python" or string.find(tab.active_pane.title, "python") then
+    icon = TAB_ICON_PYTHON
+    icon_foreground = TAB_ICON_COLOR_PYTHON
+  elseif tab.active_pane.title == "node" or string.find(tab.active_pane.title, "node") then
+    icon = TAB_ICON_NODE
+    icon_foreground = TAB_ICON_COLOR_NODE
+  elseif tab.active_pane.title == "docker" or string.find(tab.active_pane.title, "docker") then
+    icon = TAB_ICON_DOCKER
+    icon_foreground = TAB_ICON_COLOR_DOCKER
+  elseif tab.active_pane.title == "task" or string.find(tab.active_pane.title, "task") then
+    icon = TAB_ICON_TASK
+    icon_foreground = TAB_ICON_COLOR_TASK
+  end
+
+  local title = " " .. wezterm.truncate_right(cwd, max_width) .. " "
   return {
     { Background = { Color = edge_background } },
+    { Text = " " },
     { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
+    { Text = SOLID_LEFT_CIRCLE },
+    { Background = { Color = edge_foreground } },
+    { Foreground = { Color = icon_foreground } },
+    { Text = icon },
     { Background = { Color = background } },
     { Foreground = { Color = foreground } },
     { Text = title },
     { Background = { Color = edge_background } },
     { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
+    { Text = SOLID_RIGHT_CIRCLE },
   }
 end)
 
