14e9264475f19d594b1eb19cd96580a3dc3edaba
mozumasu <ouri1229@gmail.com>
2025-02-28 17:35:29 +0900

    feat(nvim): ✨ add Python configuration and new vim-rfc plugin support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- python
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+vim.g.python_host_prog = "/opt/homebrew/bin/python3"
