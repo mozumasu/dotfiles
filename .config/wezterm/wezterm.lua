214e2358e74a4ae3e6d6ee0f1f5bf797acfff9e2
mozumasu <ouri1229@gmail.com>
2025-03-19 12:27:39 +0900

    feat(wezterm): ✨ refactor key bindings for improved usability and consistency

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 local color = require("color")
+local keymaps = require("keymaps")
 color.apply_to_config(config)
+keymaps.apply_to_config(config)
 
 config.font_size = 13.0
 config.font = wezterm.font("HackGen Console NF")
