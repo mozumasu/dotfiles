37277841e23f12d69ae0bff760fa28dc4380be62
mozumasu <ouri1229@gmail.com>
2025-03-24 09:20:18 +0900

    feat(wezterm): ✨ add workspace functionality with dynamic key bindings
    wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 local color = require("color")
 local keymaps = require("keymaps")
 local appearance = require("appearance")
+local workspace = require("workspace")
 color.apply_to_config(config)
 keymaps.apply_to_config(config)
 
@@ -11,5 +12,6 @@ config.font_size = 13.0
 config.font = wezterm.font("HackGen Console NF")
 functions.apply_to_config(config)
 appearance.apply_to_config(config)
+workspace.apply_to_config(config)
 
 return config
