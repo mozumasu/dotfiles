d3af4fa5380daa1633a78e6a2b560592dde4fa40
mozumasu <ouri1229@gmail.com>
2025-03-24 13:11:03 +0900

    feat(wezterm): ✨ add workspace keybind

--- .config/wezterm/functions.lua
+++ .config/wezterm/functions.lua
@@ -4,11 +4,11 @@ local module = {}
 
 function module.apply_to_config(config)
   -- 透明度調整のキーを追加
-  table.insert(config.key_tables.setting_mode, { key = ";", action = act({ EmitEvent = "increase-opacity" }) })
-  table.insert(config.key_tables.setting_mode, { key = "-", action = act({ EmitEvent = "decrease-opacity" }) })
-  table.insert(config.key_tables.setting_mode, { key = "0", action = act({ EmitEvent = "reset-opacity" }) })
-  table.insert(config.key_tables.setting_mode, { key = "9", action = act({ EmitEvent = "reset-color" }) })
-  table.insert(config.key_tables.setting_mode, { key = "8", action = act({ EmitEvent = "toggle-color-scheme" }) })
+  table.insert(config.key_tables.config_mode, { key = ";", action = act({ EmitEvent = "increase-opacity" }) })
+  table.insert(config.key_tables.config_mode, { key = "-", action = act({ EmitEvent = "decrease-opacity" }) })
+  table.insert(config.key_tables.config_mode, { key = "0", action = act({ EmitEvent = "reset-opacity" }) })
+  table.insert(config.key_tables.config_mode, { key = "9", action = act({ EmitEvent = "reset-color" }) })
+  table.insert(config.key_tables.config_mode, { key = "8", action = act({ EmitEvent = "toggle-color-scheme" }) })
 end
 
 -- opacity
@@ -22,9 +22,9 @@ wezterm.on("decrease-opacity", function(window)
     overrides.window_background_opacity = 0.1
   end
   window:set_config_overrides(overrides)
-  -- ここで `setting_mode` を再アクティブ化
+  -- ここで `config_mode` を再アクティブ化
   window:perform_action(
-    wezterm.action.ActivateKeyTable({ name = "setting_mode", one_shot = false }),
+    wezterm.action.ActivateKeyTable({ name = "config_mode", one_shot = false }),
     window:active_pane()
   )
 end)
@@ -39,9 +39,9 @@ wezterm.on("increase-opacity", function(window)
     overrides.window_background_opacity = 1.0
   end
   window:set_config_overrides(overrides)
-  -- ここで `setting_mode` を再アクティブ化
+  -- ここで `config_mode` を再アクティブ化
   window:perform_action(
-    wezterm.action.ActivateKeyTable({ name = "setting_mode", one_shot = false }),
+    wezterm.action.ActivateKeyTable({ name = "config_mode", one_shot = false }),
     window:active_pane()
   )
 end)
@@ -50,9 +50,9 @@ wezterm.on("reset-opacity", function(window, config)
   local overrides = window:get_config_overrides() or {}
   overrides.window_background_opacity = config.window_background_opacity
   window:set_config_overrides(overrides)
-  -- ここで `setting_mode` を再アクティブ化
+  -- ここで `config_mode` を再アクティブ化
   window:perform_action(
-    wezterm.action.ActivateKeyTable({ name = "setting_mode", one_shot = false }),
+    wezterm.action.ActivateKeyTable({ name = "config_mode", one_shot = false }),
     window:active_pane()
   )
 end)
