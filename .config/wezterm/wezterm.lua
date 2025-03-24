e0dab419940746ee96822e4070783d1ec7bcc915
mozumasu <ouri1229@gmail.com>
2025-03-24 15:13:16 +0900

    feat(wezterm): ✨ enable automatic config reload and adjust settings for IME

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,18 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.automatically_reload_config = true
+config.audible_bell = "Disabled"
+-- 日本語を入力するのに必要
+config.use_ime = true
+
 local color = require("color")
 local keymaps = require("keymaps")
+local functions = require("functions")
 local appearance = require("appearance")
 local workspace = require("workspace")
 color.apply_to_config(config)
 keymaps.apply_to_config(config)
-
-config.font_size = 13.0
-config.font = wezterm.font("HackGen Console NF")
 functions.apply_to_config(config)
 appearance.apply_to_config(config)
 workspace.apply_to_config(config)
