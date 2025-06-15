55b8c5a28378a8f749262ae8a9347461cfbb84fe
mozumasu <ouri1229@gmail.com>
2025-06-15 10:28:44 +0900

    feat(zsh): ✨ update Sheldon plugins and add new Zeno snippets and abbreviations

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -13,53 +13,12 @@ fi
 alias ...='../../'
 alias ....='../../../'
 alias .....='../../../../'
-alias -g G='| grep --color=auto'
-abbr -S -qq -='cd -'
-abbr -S -qq ll='ls -l'
-abbr -S -qq la='ls -A'
-abbr -S -qq lla='ls -l -A'
-abbr -S -qq r='exec $SHELL -l'
-abbr -S -qq myip='curl ifconfig.me'
-abbr -S -qq dhosts='nvim ~/.ssh/conf.d/hosts/'
-abbr -S -qq hosts='sudo nvim /etc/hosts'
-
-# git
-abbr -S -qq g='git'
-abbr -S -qq gst='git status'
-abbr -S -qq gsw='git switch'
-abbr -S -qq gbr='git branch'
-abbr -S -qq gfe='git fetch'
-abbr -S -qq gpl='git pull'
-abbr -S -qq gad='git add'
-abbr -S -qq gcm='git commit'
-abbr -S -qq gmg='git merge'
-abbr -S -qq gpsh='git push'
-abbr -S -qq lg='lazygit'
-abbr -S -qq proot='cd $(git rev-parse --show-toplevel)'
-
-# configure file
-alias zshconf="nvim ~/.zshrc"
-alias szsh="source ~/dotfiles/.config/zsh/.zshrc"
 
 # rm
 [[ $(command -v gomi) ]] && alias gm='gomi'
 
-# zenn
-alias zenn='nvim ~/src/private/zenn'
-
-# infra
-alias mul='multipass'
-abbr -S -qq ap='ansible-playbook'
-abbr -S -qq tf='terraform'
-abbr -S -qq tfi='terraform init'
-abbr -S -qq tfp='terraform plan'
-abbr -S -qq tfa='terraform apply'
-abbr -S -qq tfs='terraform state'
-abbr -S -qq tfsl='terraform state list'
 
 # aws
-abbr -S -qq awsl='aws configure list'
-abbr -S -qq awsm='aws-mfa'
 alias awsp='set_aws_profile'
 alias awsvl='aws_vault_login'
 alias awsv='aws-vault'
@@ -81,13 +40,6 @@ alias svnr='svn st | grep "^!" | awk "{print $2}" | xargs svn rm'
 alias sshf='fzf-ssh'
 alias gi='create_gitignore'
 
-# python
-[[ $(command -v python3) ]] && abbr -S -qq py='python3'
-[[ $(command -v uv) ]] && abbr -S -qq upy='uv run python'
-
-# Laravel sail
-alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
-
 # Claude
 alias claudeconfig='v ~/Library/Application\ Support/Claude/claude_desktop_config.json'
 
@@ -101,22 +53,13 @@ hash -d nvimplugins=$XDG_DATA_HOME/nvim/lua
 # ----------------------------------------------------
 # for Mac
 # ----------------------------------------------------
-abbr -S -qq f='open .'
-
 # karabiner
 alias killkara='sudo killall karabiner_grabber'
 
 # vpnutil
-abbr -S -qq vpn='vpnutil'
 alias vpns='check_vpn_status'
 alias vpnc='vpn_connect_with_fzf'
 alias vpnd='vpn_disconnect_if_connected'
 
 # notification
 alias beep='for i in {1..3}; do afplay /System/Library/Sounds/Submarine.aiff; done'
-
-
-# ----------------------------------------------------
-# for Mac
-# ----------------------------------------------------
-abbr -S -qq p_rm='python3 ~/src/github.com/mozumasu/python/article/rename_cleanup.py'
