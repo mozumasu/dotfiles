0f159b60621fdc041a38fb023b26fbc758b9e159
mozumasu <ouri1229@gmail.com>
2025-08-22 15:23:57 +0900

    refactor(zsh): ♻️ update Zeno plugin configuration for improved command previews

--- .config/zsh/rc/pluginconfig/zeno.zsh
+++ .config/zsh/rc/pluginconfig/zeno.zsh
@@ -16,14 +16,10 @@ export ZENO_ENABLE_SOCK=1
 # if disable builtin completion
 # export ZENO_DISABLE_BUILTIN_COMPLETION=1
 
-# default
-# export ZENO_GIT_CAT="cat"
-# git file preview with color
+# default: cat
 export ZENO_GIT_CAT="bat --color=always"
 
-# default
-# export ZENO_GIT_TREE="tree"
-# git folder preview with color
+# default: tree
 export ZENO_GIT_TREE="exa --tree"
 
 if [[ -n $ZENO_LOADED ]]; then
@@ -40,23 +36,18 @@ if [[ -n $ZENO_LOADED ]]; then
   bindkey '^i' zeno-completion
 
   bindkey '^x '  zeno-insert-space
+
+  # Run the current command line without snippet expansion
   bindkey '^x^m' accept-line
+  # Toggle automatic snippet expansion on and off
   bindkey '^x^z' zeno-toggle-auto-snippet
 
   # fallback if completion not matched
   # (default: fzf-completion if exists; otherwise expand-or-complete)
   # export ZENO_COMPLETION_FALLBACK=expand-or-complete
  
-  # ZLE widget
   bindkey '^r'   zeno-history-selection
   bindkey '^x^s' zeno-insert-snippet
-  bindkey '^x^f' zeno-ghq-cd
-
-fi
-
-if [[ -n $ZENO_LOADED ]]; then
-  bindkey '^r'   zeno-history-selection
-  bindkey '^x^s' zeno-insert-snippet
-  bindkey '^x^f' zeno-ghq-cd
-  bindkey '^x^n'   zeno-snippet-next-placeholder
+  bindkey '^x^f' zeno-snippet-next-placeholder
+  # bindkey '^x^f' zeno-ghq-cd
 fi
