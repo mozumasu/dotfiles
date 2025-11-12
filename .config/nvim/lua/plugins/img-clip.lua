353beaa7448fe3c77bf3b4d398b0e6144352a962
mozumasu <ouri1229@gmail.com>
2025-11-12 15:00:00 +0900

    feat(nvim): ✨ add default options for img-clip plugin configuration

--- .config/nvim/lua/plugins/img-clip.lua
+++ .config/nvim/lua/plugins/img-clip.lua
@@ -2,8 +2,17 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+    default = {
+      -- file and directory options
+      dir_path = "assets", ---@type string | fun(): string
+      extension = "png", ---@type string | fun(): string
+      use_absolute_path = false, ---@type boolean | fun(): boolean
+      relative_to_current_file = true, ---@type boolean | fun(): boolean
+
+      -- prompt options
+      prompt_for_file_name = true, ---@type boolean | fun(): boolean
+      show_dir_path_in_prompt = false, ---@type boolean | fun(): boolean
+    },
   },
   keys = {
     -- suggested keymap
