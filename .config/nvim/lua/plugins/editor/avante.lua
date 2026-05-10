cf4ecf5240f255b4919062ce5cfdaecd0692a835
mozumasu <ouri1229@gmail.com>
2026-05-10 13:12:16 +0900

    chore: 🔨 refactor plugin configuration and disable unused options

--- .config/nvim/lua/plugins/editor/avante.lua
+++ .config/nvim/lua/plugins/editor/avante.lua
@@ -2,9 +2,6 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- set this if you want to always pull the latest change
-  opts = {
-    provider = "copilot",
-  },
   keys = { "<leader>a", desc = "Avante" },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
@@ -15,7 +12,6 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
