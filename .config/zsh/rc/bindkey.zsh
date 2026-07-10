b577917984c2feb066924302e91803d46e9abbb5
mozumasu <ouri1229@gmail.com>
2026-07-10 15:43:58 +0900

    feat: ✨ bind arrow keys to history-beginning-search
    roBa の ZMK firmware で Ctrl+N/P を Down/Up 矢印に変換する運用に合わせ、
    矢印キーにも Ctrl+N/P と同じ history-beginning-search-*-end を割り当てる。
    WezTerm など Karabiner を通らないアプリでも roBa 経由の矢印がヒストリ検索
    として機能し、物理矢印キーからも同じ挙動が得られる。

--- .config/zsh/rc/bindkey.zsh
+++ .config/zsh/rc/bindkey.zsh
@@ -30,6 +30,8 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^p" history-beginning-search-backward-end
 bindkey "^n" history-beginning-search-forward-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
 
 # Smart Insert Last Word
 autoload smart-insert-last-word
